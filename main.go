626b2a5213ea83deeec29291bc9e7eb8fb841ab6
rhodeon <ruonabraks@gmail.com>
2022-03-17 06:28:40 +0100

    Implemented functions for parsing expressions without parentheses.

--- main.go
+++ main.go
@@ -3,23 +3,188 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
+	"strconv"
+	"strings"
 )
 
 var ErrMalformedExp = errors.New("malformed expression")
+var ErrIllegalCharacter = errors.New("illegal character detected")
 
+// operators
 const (
 	openParenthesis  = "("
 	closeParenthesis = ")"
+	add              = "+"
+	subtract         = "-"
+	multiply         = "*"
+	divide           = "/"
+	decimal          = "."
 )
 
 func main() {
-	max, err := maxDepth("()()()")
+	res, err := normalize("+ 2/ 3")
 	if err != nil {
-		log.Fatalln(err.Error())
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("%#v", res)
+	}
+}
+
+func isLegal(value string) bool {
+	switch value {
+	case add, subtract, multiply, divide:
+		return true
+	default:
+		_, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return false
+		}
+		return true
+	}
+}
+
+// simplify reduces an expression to have only addition and subtraction operators.
+// An error is returned if a non-digit or non-operator character is found.
+func simplify(expr string) ([]string, error) {
+	// remove whitespaces and guard against illegal character placements
+	legalExpr, err := normalize(expr)
+	if err != nil {
+		return []string{}, err
 	}
 
-	fmt.Printf("Max depth: %d", max)
+	var simplified []string
+	var skipFlag bool
+
+	for pos, value := range legalExpr {
+		switch value {
+		// simplify multiplications and divisions by
+		// evaluating the preceding and succeeding operands
+		case multiply, divide:
+
+			// all ParseFloats are guaranteed to work as illegal characters have
+			// already been guarded against
+			prev, _ := strconv.ParseFloat(simplified[len(simplified)-1], 64)
+			next, _ := strconv.ParseFloat(legalExpr[pos+1], 64)
+
+			// replace previously added value in legalExp with the multiplication/division evaluation
+			if value == multiply {
+				simplified[len(simplified)-1] = strconv.FormatFloat(prev*next, 'f', -1, 64)
+			} else {
+				simplified[len(simplified)-1] = strconv.FormatFloat(prev/next, 'f', -1, 64)
+			}
+			skipFlag = true
+
+		default:
+			// skip current operand if previous operation was multiplication or division
+			// as it has already been evaluated
+			if !skipFlag {
+				simplified = append(simplified, value)
+			}
+			skipFlag = false
+		}
+	}
+
+	return simplified, nil
+}
+
+// normalize removes all whitespaces from the expression and returns a list of the remaining valid values.
+// An error is returned if a non-digit or non-operator character is found.
+// An error is returned if the expression begins with a multiplication, division or decimal.
+// An error is returned if the expression begins with an addition, subtraction, multiplication, division or decimal.
+func normalize(expr string) ([]string, error) {
+	// remove whitespaces in expression
+	trimmedExpr := strings.Join(strings.Fields(expr), "")
+	var legalExp []string
+
+	// TODO: Guard against consecutive operators
+
+	// the operand is accumulated and added to legalExp when an operator is found
+	var operand string
+
+	for pos, char := range trimmedExpr {
+		value := string(char)
+
+		if !isLegal(value) && value != decimal {
+			return []string{}, ErrIllegalCharacter
+		}
+
+		if pos == 0 {
+			switch value {
+			// allow only addition and subtraction as starting operators
+			case add, subtract:
+				legalExp = append(legalExp, value)
+
+			case multiply, divide, decimal:
+				return []string{}, ErrIllegalCharacter
+
+			default:
+				operand += value
+			}
+		} else if pos == len(trimmedExpr)-1 {
+			switch value {
+			case add, subtract, multiply, divide, decimal:
+				// operators cannot end an expression
+				return []string{}, ErrIllegalCharacter
+
+			default:
+				// accumulate final operand and append to legalExp
+				operand += value
+				legalExp = append(legalExp, operand)
+			}
+		} else {
+			switch value {
+			case add, subtract, multiply, divide:
+				// append and reset operand
+				legalExp = append(legalExp, operand)
+				operand = ""
+				// append operator
+				legalExp = append(legalExp, value)
+
+			default:
+				// accumulate operand
+				operand += value
+			}
+		}
+	}
+
+	return legalExp, nil
+}
+
+// evaluate computes a simplified expression list (with only addition and subtraction operators)
+// into a final float result.
+func evaluate(expr []string) float64 {
+	var result float64
+
+	// the flags determine the next operation
+	var addFlag = true
+	var subtractFlag bool
+
+	for _, value := range expr {
+		switch value {
+		case add:
+			addFlag = true
+			subtractFlag = false
+
+		case subtract:
+			subtractFlag = true
+			addFlag = false
+
+		default:
+			valueDigit, _ := strconv.ParseFloat(value, 64)
+
+			// add or subtract based on active flag
+			if addFlag {
+				result = result + valueDigit
+			} else if subtractFlag {
+				result = result - valueDigit
+			}
+
+			addFlag = false
+			subtractFlag = false
+		}
+	}
+
+	return result
 }
 
 // maxDepth returns the deepest level of nested brackets an expression has.
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_maxDepth(t *testing.T) {
 	tests := []struct {
@@ -31,6 +34,65 @@ func Test_maxDepth(t *testing.T) {
 
 			if depth != tt.wantDepth {
 				t.Errorf("\nGot:\t%d\nWant:\t%d", depth, tt.wantDepth)
+			}
+		})
+	}
+}
+
+func Test_simplify(t *testing.T) {
+	tests := []struct {
+		name       string
+		expression string
+		wantResult []string
+		wantErr    error
+	}{
+		{"pure addition", "3 + 5 + 8", []string{"3", "+", "5", "+", "8"}, nil},
+		{"pure subtraction", "3 - 5 - 8", []string{"3", "-", "5", "-", "8"}, nil},
+		{"pure multiplication", "3 * 5.5 * 8", []string{"132"}, nil},
+		{"pure division", "3 / 5 / 8", []string{"0.075"}, nil},
+		{"mixed", "3 + 5 - 2 * 7 / 3 + 4", []string{"3", "+", "5", "-", "4.666666666666667", "+", "4"}, nil},
+		{"illegal character", "3 + b - 2", []string{}, ErrIllegalCharacter},
+		{"addition prefix", "+ 2 / 3", []string{"+", "0.6666666666666666"}, nil},
+		{"subtraction prefix", "- 2 * 3", []string{"-", "6"}, nil},
+		{"multiplication prefix", "* 2 * 3", []string{}, ErrIllegalCharacter},
+		{"division prefix", "/ 2 * 3", []string{}, ErrIllegalCharacter},
+		{"end with operation", "2 * 3 +", []string{}, ErrIllegalCharacter},
+		{"joined operands", "2*4", []string{"8"}, nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := simplify(tt.expression)
+
+			if err != tt.wantErr {
+				t.Errorf("\nGot:\t%v\nWant:\t%v", err, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(result, tt.wantResult) {
+				t.Errorf("\nGot:\t%#v\nWant:\t%#v", result, tt.wantResult)
+			}
+		})
+	}
+}
+
+func Test_evaluate(t *testing.T) {
+	tests := []struct {
+		name       string
+		expression []string
+		result     float64
+	}{
+		{"addition only", []string{"8", "+", "6"}, 14},
+		{"subtraction only", []string{"8", "-", "6"}, 2},
+		{"addition prefix", []string{"+", "2", "-", "5.2"}, -3.2},
+		{"subtraction prefix", []string{"-", "2", "+", "5.8"}, 3.8},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := evaluate(tt.expression)
+
+			if result != tt.result {
+				t.Errorf("\nGot:\t%f\nWant:\t%f", result, tt.result)
 			}
 		})
 	}
