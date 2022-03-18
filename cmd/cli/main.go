ce4c1ca771734424846f7e55a68c216804946490
rhodeon <ruonabraks@gmail.com>
2022-03-19 00:06:28 +0100

    Established CLI package.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/rhodeon/expression-parser/pkg/solver"
 	"log"
 )
 
 func main() {
-	expr := "2+5-2(6+4)+3"
-	_, err := solver.Validate(expr)
+	expr := flag.String("expr", "1 + 2", "expression to solve")
+	flag.Parse()
+
+	_, err := solver.Validate(*expr)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	result, err := solver.Solve(expr)
+	result, err := solver.Solve(*expr)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Printf("%#v", result)
+	fmt.Printf("%s = %g\n", *expr, result)
 }
