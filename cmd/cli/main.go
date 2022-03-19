fe2ee780ee4c4db369c6f63d5ae4f71190517047
rhodeon <ruonabraks@gmail.com>
2022-03-19 01:59:25 +0100

    Added a help message on an empty CLI flag.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"github.com/rhodeon/expression-parser/pkg/solver"
 	"log"
+	"os"
 )
 
 func main() {
-	expr := flag.String("expr", "1 + 2", "expression to solve")
+	expr := flag.String("expr", "", "expression to solve")
 	flag.Parse()
+
+	if *expr == "" {
+		println("Exparse: Input an expression with the 'expr' flag")
+		os.Exit(0)
+	}
 
 	_, err := solver.Validate(*expr)
 	if err != nil {
@@ -17,5 +23,5 @@ func main() {
 	}
 
 	result := solver.Solve(*expr)
-	fmt.Printf("%s = %g\n", *expr, result)
+	fmt.Printf("Exparse: %s = %g\n", *expr, result)
 }
