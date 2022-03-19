13465209aa14c95272957bcdf2436bd1f2fdeceb
rhodeon <ruonabraks@gmail.com>
2022-03-19 04:03:43 +0100

    Added network address flag.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"flag"
 	"github.com/rhodeon/prettylog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "4000"
+	}
+	addr := flag.String("addr", ":"+port, "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/static/", serveStaticFiles)
 	mux.HandleFunc("/result", calculateResult)
 
-	server := http.Server{Addr: ":4000", Handler: mux}
+	server := http.Server{Addr: *addr, Handler: mux}
 	prettylog.InfoF("Starting server on %s", server.Addr)
 	err := server.ListenAndServe()
 	prettylog.FatalError(err)
